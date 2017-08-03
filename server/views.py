2f9c0d3b91421b3d3b40387e24a4a1dec92eb20d
simzipark <bzh0406@naver.com>
2017-08-03 00:29:13 +0000

    using def

--- server/views.py
+++ server/views.py
@@ -8,7 +8,7 @@ from django.views.decorators.csrf import csrf_exempt
 def keyboard(request):
 	data = {
 		"type": "buttons",
-		"buttons": ["강다니엘", "옹성우"]
+		"buttons": ["강다니엘", "옹성우", "워너원"]
 	}
 	
 	return HttpResponse(json.dumps(data, ensure_ascii=False), content_type="application/json; encoding=utf-8")
@@ -21,15 +21,12 @@ def message(request):
 	body = json.loads(body_unicode)
 	content = body['content']
 	
+	chat_room(request)
+	
 	if content == u"강다니엘":
 		data = {	
 			"message": {
 				"text": content+" Pick!",
-				"photo": {
-					"url": "http://post.phinf.naver.net/MjAxNzA2MThfMTQ5/MDAxNDk3NzYwMTQ3Njcx.g5Yh4qZM5d-IsFLqUJIAk2ICGdugE-QWTgk298e_-G4g.VnXqA8m4GMeMKAvQ7ACrP-K9sGJ_4hazfCev3uQA09Ag.JPEG/IibflNMZDZF3xlkrIWObOayz6FEI.jpg",
-					"width": 850,
-					"height": 1275
-				},
 				"message_button": {
 					"label": "WANNAONE GO!",
 					"url": "http://www.wannaonego.com"
@@ -37,56 +34,37 @@ def message(request):
 			},
 			"keyboard": {
 				"type": "buttons",
-				"buttons": ["강다니엘", "옹성우", "메인으루"]
+				"buttons": ["강다니엘", "옹성우", "워너원"]
 			}
 		}
+	elif content == u"워너원":
+		data = chat_room(request)
 	elif content == u"옹성우":
 		data = {		
 			"message": {
 				"text": content+" Pick!",
-				"photo": {
-					"url": "http://cafefiles.naver.net/MjAxNzA3MDJfNzcg/MDAxNDk4OTI3NjQ4OTU0.-rtgs90xqWUjg5bROJkGd2MrZiyFKDVO6WD3hj2gQtUg.a9lofCKrWC1QekHdldeeR47_iUzN0lTLXw9g-UHaHvEg.JPEG.jeonginhye06/externalFile.jpg",
-					"width": 960,
-					"height": 1402
-				},
 				"message_button": {
 					"label": "WANNAONE GO!",
 					"url": "http://www.wannaonego.com"
-				},
+				}
 			},
 			"keyboard": {
 				"type": "buttons",
-				"buttons": ["강다니엘", "옹성우", "메인으루"]
+				"buttons": ["강다니엘", "옹성우", "워너원"]
 			}
 		}
 	else:
 		data = {
 			"message": {
-				"text" : content
+				"text" : "All I Wanna Do! " + content
 			},
 			"keyboard": {
 				"type": "buttons",
-				"buttons": ["강다니엘", "옹성우"]
+				"buttons": ["강다니엘", "옹성우", "워너원"]
 			}
 		}
 	
 	return HttpResponse(json.dumps(data, ensure_ascii=False), content_type="application/json; encoding=utf-8")
 
-
 def chat_room(request):
-	data = {
-		'message': {
-			'text': 'test'
-		}
-	}
-	
-	return HttpResponse(json.dumps(data, ensure_ascii=False), content_type='application/json; encoding=utf-8')
-
-def friend(request):
-	data = {
-		'message': {
-			'text': 'test'
-		}
-	}
-
-	return HttpResponse(json.dumps(data, ensure_ascii=False), content_type='application/json; encoding=utf-8')
+	return	{"message":{"text" : "19961210"},"keyboard":{"type": "buttons","buttons": ["강다니엘", "옹성우", "워너원"]}}
--- simzi/urls.py
+++ simzi/urls.py
@@ -5,6 +5,4 @@ from server import views
 urlpatterns = [
 	url(r'keyboard', views.keyboard),
 	url(r'message', views.message),
-	url(r'chat_room', views.chat_room),
-	url(r'friend', views.friend),
 ]
