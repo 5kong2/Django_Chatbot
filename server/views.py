79f68a30eef9ba1f78b54c70870ca0a4e3f6af69
simzipark <bzh0406@naver.com>
2017-08-03 01:02:45 +0000

    def

--- server/views.py
+++ server/views.py
@@ -21,32 +21,14 @@ def message(request):
 	body = json.loads(body_unicode)
 	content = body['content']
 	
-	chat_room(request)
+	data = pick(request)
 	
-	if content == u"강다니엘":
-		data = {	
+	if content == u"워너원":
+		data = wannaone(request)
+	elif content == u"처음으로":
+		data = {
 			"message": {
-				"text": content+" Pick!",
-				"message_button": {
-					"label": "WANNAONE GO!",
-					"url": "http://www.wannaonego.com"
-				}
-			},
-			"keyboard": {
-				"type": "buttons",
-				"buttons": ["강다니엘", "옹성우", "워너원"]
-			}
-		}
-	elif content == u"워너원":
-		data = chat_room(request)
-	elif content == u"옹성우":
-		data = {		
-			"message": {
-				"text": content+" Pick!",
-				"message_button": {
-					"label": "WANNAONE GO!",
-					"url": "http://www.wannaonego.com"
-				}
+				"text" : "당신의 소년을 선택해주세요!",
 			},
 			"keyboard": {
 				"type": "buttons",
@@ -54,17 +36,38 @@ def message(request):
 			}
 		}
 	else:
-		data = {
-			"message": {
+		data = pick(request)
+	
+	return HttpResponse(json.dumps(data, ensure_ascii=False), content_type="application/json; encoding=utf-8")
+
+def pick(request):
+	body_unicode = request.body.decode('utf-8')
+	body = json.loads(body_unicode)
+	content = body['content']
+	
+	return {
+		"message": {
+				"text" : content+" pick!",
+				"message_button": {
+					"label": "WANNAONE GO!",
+					"url": "http://www.wannaonego.com"
+				}
+			},
+			"keyboard": {
+				"type": "buttons",
+				"buttons": ["강다니엘", "옹성우", "워너원", "처음으로"]
+			}
+	}
+
+def wannaone(request):
+	body_unicode = request.body.decode('utf-8')
+	body = json.loads(body_unicode)
+	content = body['content']
+	return	{"message": {
 				"text" : "All I Wanna Do! " + content
 			},
 			"keyboard": {
 				"type": "buttons",
-				"buttons": ["강다니엘", "옹성우", "워너원"]
+				"buttons": ["강다니엘", "옹성우", "워너원", "처음으로"]
 			}
-		}
-	
-	return HttpResponse(json.dumps(data, ensure_ascii=False), content_type="application/json; encoding=utf-8")
-
-def chat_room(request):
-	return	{"message":{"text" : "19961210"},"keyboard":{"type": "buttons","buttons": ["강다니엘", "옹성우", "워너원"]}}
+			}
